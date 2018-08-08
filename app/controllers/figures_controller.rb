448834813dd1353048450f32a298cdda28e821cb
kreyes12 <karla.y.reyes12@gmail.com>
2018-08-08 15:50:01 +0100

    nyc sinatra fun

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,44 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  post '/figures' do
+    figure = Figure.create(params[:figure])
+    title = Title.create(params[:title])
+    landmark = Landmark.create(params[:landmark])
+    redirect '/figures/#{figure.id}'
+  end
+
+  get '/figures/:id/edit' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.update(params[:figure])
+    redirect '/figures/#{figure.id}'
+  end
+
+
+
+
+
+
+end
